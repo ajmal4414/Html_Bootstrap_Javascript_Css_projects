00e22152568a38751489ff1e702750f872b615b6
ajmal4414 <ajmalsckl@gmail.com>
2023-01-29 22:03:03 +0530

    first commit

---
+++ sampl.js
@@ -0,0 +1,27 @@
+//document.write("learn javascript");
+//alert("hello world");
+console.log("hii world")
+document.write
+
+/*
+hhhhghghg
+hjjhjhjh
+*/
+
+//variable & datatypes
+//str
+//var person="tom";
+//let person1="john"
+//document.write(person)
+//document.write("<br>")
+//document.write(typeof(person))
+
+//let a=6
+//document.write(a)
+var x = myFunction(4,2);
+document.getElement
+function myFunction(a,b){
+return a*b;
+}
+//let z = x + y;
+//document.write(let)
